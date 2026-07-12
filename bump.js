9f2285178f197a5f091e15b649245d40082cb270
averoes67 <averoes67@users.noreply.github.com>
2026-07-12 19:31:00 +0700

    Clean up temp scripts

--- bump.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const htmlFiles = fs.readdirSync('.').filter(f => f.endsWith('.html'));
-
-for (const file of htmlFiles) {
-    let content = fs.readFileSync(file, 'utf8');
-    content = content.replace(/href="css\/style\.css\?v=2"/g, 'href="css/style.css?v=3"');
-    fs.writeFileSync(file, content);
-}
-console.log('Bumped cache version');
